d824055719819ee584becf764c7bebc03efd91b5
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-03-24 09:10:22 -0400

    123

---
+++ echo_client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define BUFLEN 1024
+
+int main()
+{
+    int sd;
+    struct sockaddr_in server, client;
+    int client_len;
+
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    connect(sd, (struct sockaddr *)&server, sizeof(server));
+
+    char sbuf[BUFLEN], rbuf[BUFLEN];
+    int n, i, bytes_to_read;
+    char *bp;
+    printf("Transmit: \n");
+    while (n = read(0, sbuf, BUFLEN))
+    {                       /* get user message */
+        write(sd, sbuf, n); /* send it out */
+        printf("Receive: \n");
+        bp = rbuf;
+        bytes_to_read = n;
+        while ((i = read(sd, bp, bytes_to_read)) > 0)
+        {
+            bp += i;
+            bytes_to_read -= i;
+        }
+        write(1, rbuf, n);
+        printf("Transmit: \n");
+    }
+    close(sd);
+}
---
+++ echo_server.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define BUFLEN 1024
+
+int echod(int sd)
+{
+    char *bp, buf[BUFLEN];
+    int n, bytes_to_read;
+    while(n = read(sd, buf, BUFLEN))
+    write(sd, buf, n);
+    close(sd);
+    return(0);
+} 
+
+int main()
+{
+    int sd, new_sd;
+    struct sockaddr_in server, client;
+    int client_len;
+
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(sd, (struct sockaddr *)&server, sizeof(server));         // binds socket to IP and port
+    listen(sd, 5);                                                // puts socket into passive mode and accepts data of size 5
+    new_sd = accept(sd, (struct sockaddr *)&client, &client_len); // accepts connection from client
+
+    switch (fork())
+    {
+    case 0: /* child */
+        (void) close(sd);
+        exit(echod(new_sd));
+    default: /* parent */
+        (void) close(new_sd);
+        break;
+    case -1:
+        fprintf(stderr, "fork: error\n");
+    }
+}
